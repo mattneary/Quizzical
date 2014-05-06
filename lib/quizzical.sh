781ab01f309aca49533facf73962e2c220dc4123
Matt Neary <neary.matt@gmail.com>
2014-05-05 22:28:13 -0400

    delayed error output and dumping of error page

--- lib/quizzical.sh
+++ lib/quizzical.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 QUIZZICAL_WAREHOUSE_DIR="${QUIZZICAL_WAREHOUSE_DIR:-$HOME/.quizzical}"
 
 if [ -z $1 ]; then
@@ -10,7 +11,7 @@ if [ -z $1 ]; then
   echo "Run Tests:"
   echo "    quizzical tests.sh"
   echo ""
-  echo "Generate Documentation:"
+  echo "Generate Documentations:"
   echo "    quizzical tests.sh docs.md"
   exit 1
 fi
@@ -18,11 +19,21 @@ fi
 if [ -z $2 ]
 then
 	echo "Performing tests in $1..."	
+	cat /dev/null > .quizzical_error
+	rm .quizzical_dumps/* 2> /dev/null
+	mkdir .quizzical_dumps 2> /dev/null
 	$QUIZZICAL_WAREHOUSE_DIR/test $@
 	echo ""
-	echo "Tests complete."
+	if [ 0 -ne `cat .quizzical_error | wc -l` ]
+	then
+	  echo "Tests failed:"
+	  cat .quizzical_error | awk -F '|' '{print "  The failed response to \"" $1 "\" can be found at \".quizzical_dumps/" $2 "\"."}'
+	else
+	  echo "All tests passed."
+	fi
 else
 	echo "Generating docs to $2..."
 	$QUIZZICAL_WAREHOUSE_DIR/docs.sh $@	
 	echo "Docs generated."
 fi
+
--- src/render.sh
+++ src/render.sh
@@ -1,5 +1,11 @@
 read resp; 
-if [ -z "$resp" ]
-then echo -n "F" && echo "" && echo "failure for test: $1"
-else echo -n "."
+if [ "$resp" != "Passed" ]
+then
+  hash=`openssl md2 <(echo "hello") \
+      | awk '{print $2}'`
+  echo -n "F" 
+  echo "$1|$hash" >> .quizzical_error
+  echo "$1" > ".quizzical_dumps/$hash"
+else
+  echo -n "."
 fi
